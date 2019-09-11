456ea8a6b1f24e3441759dc9989c11b33470480e
Abed El-Rahim Badr <abedbadr1999@hotmail.com>
2019-09-11 23:23:36 +0200

    Initial commit

---
+++ SocketIterative/TCPEchoServer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net.Sockets;
+using System.IO;
+
+namespace SocketIterative
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpListener serverSocket = new TcpListener(6789);
+            serverSocket.Start();
+            Console.WriteLine("Server started");
+
+            while (true)
+            {
+                TcpClient connectionSocket = serverSocket.AcceptTcpClient();
+                //Socket connectionSocket = serverSocket.AcceptSocket();
+                Console.WriteLine("Server activated");
+
+                Stream ns = connectionSocket.GetStream();
+                //Stream ns = new NetworkStream(connectionSocket);
+                StreamReader sr = new StreamReader(ns);
+                StreamWriter sw = new StreamWriter(ns);
+                sw.AutoFlush = true; // enable automatic flushing
+
+                string message = sr.ReadLine();
+                string answer = "";
+                while (message != null && message != "")
+                {
+                    Console.WriteLine("Client: " + message);
+                    answer = message.ToUpper();
+                    sw.WriteLine(answer);
+                    message = sr.ReadLine();
+                }
+                ns.Close();
+                connectionSocket.Close();
+                serverSocket.Stop();
+            }
+        }
+    }
+}
