cd631069dd2198834a59fb5598b9e894864f2b20
Abed El-Rahim Badr <abedbadr1999@hotmail.com>
2019-09-11 23:36:29 +0200

    Made EchoService class public, added instance field _connectionSocket, constructor and method DoIt()

--- SocketIterative/EchoService.cs
+++ SocketIterative/EchoService.cs
@@ -1,10 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net.Sockets;
 using System.Text;
 
 namespace SocketIterative
 {
-    class EchoService
+    public class EchoService
     {
+        private TcpClient _connectionSocket;
+
+        public EchoService(TcpClient connection)
+        {
+            _connectionSocket = connection;
+        }
+
+        public void DoIt()
+        {
+
+        }
     }
 }
