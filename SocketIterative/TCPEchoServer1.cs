39722471599642dbd4005210d367e99dd2c1469c
AbxdHxcks <abedbadr1999@hotmail.com>
2019-09-12 12:30:39 +0200

    Created server socket, made infinite while-loop, inside loop created connection socket by calling accept-method, created EchoService object and called the DoIt method

--- SocketIterative/TCPEchoServer1.cs
+++ SocketIterative/TCPEchoServer1.cs
@@ -17,29 +17,10 @@ namespace SocketIterative
             while (true)
             {
                 TcpClient connectionSocket = serverSocket.AcceptTcpClient();
-                //Socket connectionSocket = serverSocket.AcceptSocket();
                 Console.WriteLine("Server activated");
 
-                Stream ns = connectionSocket.GetStream();
-                //Stream ns = new NetworkStream(connectionSocket);
-                StreamReader sr = new StreamReader(ns);
-                StreamWriter sw = new StreamWriter(ns);
-                sw.AutoFlush = true; // enable automatic flushing
-
-                string message = sr.ReadLine();
-                string answer = "";
-                while (message != null && message != "")
-                {
-                    Console.WriteLine("Client: " + message);
-                    answer = message.ToUpper();
-                    sw.WriteLine(answer);
-                    message = sr.ReadLine();
-                }
-                /*
-                ns.Close();
-                connectionSocket.Close();
-                serverSocket.Stop();
-                */
+                EchoService echoService = new EchoService(connectionSocket);
+                echoService.DoIt();
             }
         }
     }
