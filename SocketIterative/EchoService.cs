128e3021454cd91d4d1daf01074281bffd8c044d
AbxdHxcks <abedbadr1999@hotmail.com>
2019-09-12 12:26:27 +0200

    Updated DoIt method

--- SocketIterative/EchoService.cs
+++ SocketIterative/EchoService.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Net.Sockets;
 using System.Text;
+using System.IO;
 
 namespace SocketIterative
 {
@@ -16,7 +17,21 @@ namespace SocketIterative
 
         public void DoIt()
         {
+            Stream ns = _connectionSocket.GetStream();
+            //Stream ns = new NetworkStream(connectionSocket);
+            StreamReader sr = new StreamReader(ns);
+            StreamWriter sw = new StreamWriter(ns);
+            sw.AutoFlush = true; // enable automatic flushing
 
+            string message = sr.ReadLine();
+            string answer = "";
+            while (message != null && message != "")
+            {
+                Console.WriteLine("Client: " + message);
+                answer = message.ToUpper();
+                sw.WriteLine(answer);
+                message = sr.ReadLine();
+            }
         }
     }
 }
