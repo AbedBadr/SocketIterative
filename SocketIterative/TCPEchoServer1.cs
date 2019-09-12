701d71e22baa20796ae7f295da4f782d6cd26d80
AbxdHxcks <abedbadr1999@hotmail.com>
2019-09-12 11:17:40 +0200

    Copy pasted from TCPEchoServer to TCPEchoServer1

--- SocketIterative/TCPEchoServer1.cs
+++ SocketIterative/TCPEchoServer1.cs
@@ -1,11 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Net.Sockets;
+using System.IO;
 
 namespace SocketIterative
 {
     class TCPEchoServer1
     {
+        static void Main1(string[] args)
+        {
+            TcpListener serverSocket = new TcpListener(6789);
+            serverSocket.Start();
+            Console.WriteLine("Server started");
 
+            while (true)
+            {
+                TcpClient connectionSocket = serverSocket.AcceptTcpClient();
+                //Socket connectionSocket = serverSocket.AcceptSocket();
+                Console.WriteLine("Server activated");
+
+                Stream ns = connectionSocket.GetStream();
+                //Stream ns = new NetworkStream(connectionSocket);
+                StreamReader sr = new StreamReader(ns);
+                StreamWriter sw = new StreamWriter(ns);
+                sw.AutoFlush = true; // enable automatic flushing
+
+                string message = sr.ReadLine();
+                string answer = "";
+                while (message != null && message != "")
+                {
+                    Console.WriteLine("Client: " + message);
+                    answer = message.ToUpper();
+                    sw.WriteLine(answer);
+                    message = sr.ReadLine();
+                }
+                /*
+                ns.Close();
+                connectionSocket.Close();
+                serverSocket.Stop();
+                */
+            }
+        }
     }
 }
